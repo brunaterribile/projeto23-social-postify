a32f3ec66f609d244097674d32fc4face56e30e9
Bruna Terribile <brunaterribile@gmail.com>
2023-07-17 12:16:30 -0300

    feat: install prisma, bcrypt and run migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PublicationsModule } from './modules/publications/publications.module';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PublicationsModule],
+  imports: [PublicationsModule, UsersModule, PrismaModule],
 })
 export class AppModule {}
